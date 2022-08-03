9829cc2c2a8040b9dcc1dd9533d5d36cfaca33c4
ndelanhese <nnathanh3@gmail.com>
2022-08-03 16:38:55 -0300

    config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
-import { QueryClientProvider, QueryClient } from "react-query";
-import { theme } from "../src/styles/theme";
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { theme } from '../src/styles/theme';
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
-
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>
